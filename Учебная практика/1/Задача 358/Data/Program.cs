8d903d2a939a92643cbeabe677bd9a0d4d976310
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-17 15:32:12 +0500

    Добавил проверку на ноль. Добавлен правильный расчёт коэффициентов

--- Учебная практика/1/Задача 358/Data/Program.cs
+++ Учебная практика/1/Задача 358/Data/Program.cs
@@ -15,29 +15,61 @@ namespace Data
             for (int index = 0; index < tokens.Length; index++)
             {
                 coordinates[index] = int.Parse(tokens[index]);
-                }
-            double[] deltaX = new double[3];
+            }
 
+            double[] deltaX = new double[3];
+            double[] deltaY = new double[3];
             deltaX[0] = Math.Abs(coordinates[0] - coordinates[2]);
             deltaX[1] = Math.Abs(coordinates[2] - coordinates[4]);
             deltaX[2] = Math.Abs(coordinates[0] - coordinates[4]);
-
+            deltaY[0] = Math.Abs(coordinates[1] - coordinates[3]); 
+            deltaY[1] = Math.Abs(coordinates[3] - coordinates[5]);
+            deltaY[2] = Math.Abs(coordinates[1] - coordinates[5]);
             double[] coefficients = new double[3];
+
+
+
+
+
             for (int index = 0; index < 3; index++)
             {
-                
-                coefficients[index] = (double)coordinates[index * 2 + 1] / coordinates[index * 2];
-                coefficients[index] = Math.Abs(coefficients[index] - Math.Truncate(coefficients[index]));
-                if (coefficients[index] != 0)
+                if (deltaX[index] == 0)
                 {
-                    coefficients[index] = 1.0 / coefficients[index];
-                    deltaX[index] = Math.Truncate(deltaX[index] / coefficients[index]);
+                    coefficients[index] = deltaY[index];
                 }
+                else
+                {
+                    if (deltaY[index] == 0)
+                    {
+                        coefficients[index] = deltaX[index];
+                    }
+                    else
+                    {
+                        coefficients[index] = deltaY[index] / deltaX[index];
+                        coefficients[index] = Math.Abs(coefficients[index] - Math.Truncate(coefficients[index]));
+                        if (coefficients[index] != 0)
+                        {
+                            coefficients[index] = 1.0 / coefficients[index];
+                            if (coefficients[index] - Math.Truncate(coefficients[index]) > 0)
+                            {
+                                coefficients[index] = 1;
+                            }
+                            else
+                            {
+                            coefficients[index] = Math.Truncate(deltaX[index] / coefficients[index]);
+                            }
+                        }
+                        else
+                        {
+                            coefficients[index] = deltaX[index];
+                        }
+                    }
+                }
+
             }
 
-            int treeCount = (int) (deltaX[0] + deltaX[1] + deltaX[2] - 6);
+            int treeCount = (int)(coefficients[0] + coefficients[1] + coefficients[2]);
             Console.WriteLine(treeCount);
-            Console.ReadKey();
         }
     }
 }
