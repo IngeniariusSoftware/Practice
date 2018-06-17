6cfc5706969834ad626eed5c07352ddc88f3cc66
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-17 22:52:15 +0500

    Добавлена функция НОД, изменена логика на использование НОД для поиска кол-ва точек на отрезке. Задача сдана.

--- Учебная практика/1/Задача 358/Data/Program.cs
+++ Учебная практика/1/Задача 358/Data/Program.cs
@@ -1,35 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data
 {
     class Program
     {
+        static int NOD(Int32 number1, Int32 number2)
+        {
+            if (number1 == number2)
+            {
+                return number1;
+            }
+            else
+            {
+                if (number1 > number2)
+                {
+                    int shelf = number1;
+                    number1 = number2;
+                    number2 = shelf;
+                }
+
+                return NOD(number1, number2 - number1);
+            }
+        }
+
         static void Main(string[] args)
+
         {
             string[] tokens = Console.ReadLine().Split(' ');
-            int[] coordinates = new int[tokens.Length];
+            Int32[] coordinates = new Int32[tokens.Length];
             for (int index = 0; index < tokens.Length; index++)
             {
-                coordinates[index] = int.Parse(tokens[index]);
+                coordinates[index] = Int32.Parse(tokens[index]);
             }
 
-            double[] deltaX = new double[3];
-            double[] deltaY = new double[3];
+            Int32[] deltaX = new Int32[3];
+            Int32[] deltaY = new Int32[3];
             deltaX[0] = Math.Abs(coordinates[0] - coordinates[2]);
             deltaX[1] = Math.Abs(coordinates[2] - coordinates[4]);
             deltaX[2] = Math.Abs(coordinates[0] - coordinates[4]);
-            deltaY[0] = Math.Abs(coordinates[1] - coordinates[3]); 
+            deltaY[0] = Math.Abs(coordinates[1] - coordinates[3]);
             deltaY[1] = Math.Abs(coordinates[3] - coordinates[5]);
             deltaY[2] = Math.Abs(coordinates[1] - coordinates[5]);
-            double[] coefficients = new double[3];
+            decimal[] coefficients = new decimal[3];
+            bool isTriangle = true;
 
+            for (int index = 0; index < 3; index++)
+            {
+                if (deltaX[index] != 0)
+                {
+                    coefficients[index] = deltaY[index] / deltaX[index];
+                }
+            }
 
-
-
+            if (coefficients[0] == coefficients[1] && coefficients[0] == coefficients[2])
+            {
+                isTriangle = false;
+            }
 
             for (int index = 0; index < 3; index++)
             {
@@ -45,30 +71,22 @@ namespace Data
                     }
                     else
                     {
-                        coefficients[index] = deltaY[index] / deltaX[index];
-                        coefficients[index] = Math.Abs(coefficients[index] - Math.Truncate(coefficients[index]));
-                        if (coefficients[index] != 0)
-                        {
-                            coefficients[index] = 1.0 / coefficients[index];
-                            if (coefficients[index] - Math.Truncate(coefficients[index]) > 0)
-                            {
-                                coefficients[index] = 1;
-                            }
-                            else
-                            {
-                            coefficients[index] = Math.Truncate(deltaX[index] / coefficients[index]);
-                            }
-                        }
-                        else
-                        {
-                            coefficients[index] = deltaX[index];
-                        }
+                        coefficients[index] = NOD(deltaX[index], deltaY[index]);
                     }
                 }
-
             }
 
-            int treeCount = (int)(coefficients[0] + coefficients[1] + coefficients[2]);
+            Int64 treeCount;
+            Array.Sort(coefficients);
+            if (isTriangle)
+            {
+                treeCount = (Int64)(coefficients[0] + coefficients[1] + coefficients[2]);
+            }
+            else
+            {
+                treeCount = (Int64)coefficients[2] + 1;
+            }
+
             Console.WriteLine(treeCount);
         }
     }
