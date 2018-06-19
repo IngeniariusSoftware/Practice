647a969e92fa4d6934f529da527320f617a7ae36
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-19 19:18:50 +0500

    Написано решение для 3 задачи

--- Учебная практика/3/Задача 59г/Data/TaskSolution3.cs
+++ Учебная практика/3/Задача 59г/Data/TaskSolution3.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Library_Functions;
 
 namespace Data
 {
@@ -10,6 +7,28 @@ namespace Data
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\tДанная программа определяет принадлежит ли ваша точка заданной области");
+            Console.WriteLine("\n\tЕсли точка принадлежит области или лежит вне её, программа выдаст соотвествующее сообщение об этом");
+            Console.WriteLine("\n\tВвод координаты x");
+            Functions.ReadDouble(out double x);
+            Console.WriteLine("\n\tВвод координаты y");
+            Functions.ReadDouble(out double y);
+            if (x >= -1 &&
+                x <= 1 && 
+                y >= -1 && 
+                y <= 1 && 
+                x + y >= -1 && 
+                x + y <= 1)
+            {
+                Console.WriteLine("\n\tТочка ({0} , {1}) принадлежит заданной области", x, y);
+            }
+            else
+            {
+                Console.WriteLine("\n\tТочка ({0} , {1}) не лежит в заданной области", x, y);
+            }
+
+            Console.WriteLine("\n\tДля завершения работы нажмите на любую клавишу . . .");
+            Console.ReadKey();
         }
     }
 }
