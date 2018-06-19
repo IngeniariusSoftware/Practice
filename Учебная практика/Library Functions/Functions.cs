badacd4fa6f3b645efacd2ad5bd454bf9846eeaa
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-19 19:20:53 +0500

    Добавлена собственная библиотека для ввода чисел

---
+++ Учебная практика/Library Functions/Functions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Library_Functions
+{
+    public class Functions
+    {
+        public static Random Rnd = new Random();
+
+        public static void ReadNatural(out int numberNatural)
+        {
+            bool rightNatural;
+            do
+            {
+                Console.WriteLine("\n\tПожалуйста, введите натуральное число");
+                rightNatural = int.TryParse(Console.ReadLine(), out numberNatural) && numberNatural > 0;
+                if (!rightNatural)
+                {
+                    Console.WriteLine("\n\t\aК сожалению, вы ввели что-то не так. Ожидалось натуральное число");
+                    Console.WriteLine("\n\tЭто целое число, которое больше 0");
+                }
+            }
+            while (!rightNatural);
+        }
+
+        public static void ReadNonNegative(out int numberZahlen)
+        {
+            bool rightNonNegative;
+            do
+            {
+                Console.WriteLine("\n\tПожалуйста, введите неотрицательное число");
+                rightNonNegative = int.TryParse(Console.ReadLine(), out numberZahlen);
+                if (numberZahlen < 0)
+                {
+                    rightNonNegative = false;
+                }
+
+                if (!rightNonNegative)
+                {
+                    Console.WriteLine("\n\t\aК сожалению, вы ввели что-то не так. Ожидалось неотрицательное число");
+                    Console.WriteLine("\n\tЭто целое число, которое больше или равняется 0");
+                }
+            }
+            while (!rightNonNegative);
+        }
+
+        public static void ReadDouble(out double numberDouble)
+        {
+            bool rightDouble;
+            do
+            {
+                Console.WriteLine("\n\tПожалуйста, введите действительное число");
+                rightDouble = double.TryParse(Console.ReadLine(), out numberDouble);
+                if (!rightDouble)
+                {
+                    Console.WriteLine("\n\t\aК сожалению, вы ввели что-то не так. Ожидалось действительное число");
+                    Console.WriteLine("\n\tЭто любое число без посторонних символов, кроме символа ',' для ввода дробных чисел и '-' для ввода отрицательных чисел");
+                }
+            }
+            while (!rightDouble);
+        }
+
+        public static int ReadAnswer(int firstPositionOfAnswer, int lastPositionOfAnswer)
+        {
+            bool rightAnswer;
+            int numberOfAnswer;
+            do
+            {
+                Console.WriteLine(
+                    "\n\tПожалуйста, введите номер ответа. Это целое число от {0} до {1} включительно",
+                    firstPositionOfAnswer,
+                    lastPositionOfAnswer);
+                rightAnswer = int.TryParse(Console.ReadLine(), out numberOfAnswer)
+                              & numberOfAnswer <= lastPositionOfAnswer & numberOfAnswer >= firstPositionOfAnswer;
+                if (!rightAnswer)
+                {
+                    Console.WriteLine(
+                        "\n\t\aНеверный ввод. Ожидалось целое число от {0} до {1} включительно",
+                        firstPositionOfAnswer,
+                        lastPositionOfAnswer);
+                }
+            }
+            while (!rightAnswer);
+
+            return numberOfAnswer;
+        }
+    }
+}
---
+++ Учебная практика/Library Functions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Library Functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Library Functions")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("111270ca-bb3d-443f-bdf0-2842d0ae15aa")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
