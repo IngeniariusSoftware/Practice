5827a777d054332a633d573056d786d9164936d9
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-21 16:18:07 +0500

    Проинициализировано решение для 5 задачи

---
+++ Учебная практика/Задача 5/Задача692ж.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_Functions;
+namespace Задача_5
+{
+    class Задача692ж
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
